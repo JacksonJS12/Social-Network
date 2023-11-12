be5805707929d1af693db00985eba34831ae9dd6
JacksonJS12 <d.ivan0v.6100@gmail.com>
2023-11-12 23:27:31 +0200

    Initial MVC project is ready.

---
+++ Social-Network/Social-Network.Data/SocialNetworkDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Social_Network.Data
+{
+    public class SocialNetworkDbContext : IdentityDbContext
+    {
+        public SocialNetworkDbContext(DbContextOptions<SocialNetworkDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Social-Network/Social-Network.Web.ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Social_Network.Web.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
