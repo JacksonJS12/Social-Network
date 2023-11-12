744ad8489f907467f5be665c20295ef22e30a92b
JacksonJS12 <d.ivan0v.6100@gmail.com>
2023-11-13 01:00:55 +0200

    commit test

--- Social-Network/Social-Network.Web.ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Social_Network.Web.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
