e97b2a4cc94a6e60b3d58d6dcdd9c91c9a988235
JacksonJS12 <d.ivan0v.6100@gmail.com>
2023-11-17 18:11:18 +0200

    Commit

--- Blog/src/main.jsx
+++ Blog/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Navigation from './components/Navigation/Navigation.jsx'
+import HeroHeader from './components/Hero/HeroHeader.jsx'
+import Footer from './components/Footer/Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Navigation/>
+    <HeroHeader/>
+    <Footer/>
   </React.StrictMode>,
 )
