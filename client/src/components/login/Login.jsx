63c72857db142b43719b975547efb35573ba2a00
JacksonJS12 <d.ivan0v.6100@gmail.com>
2023-12-11 22:21:25 +0200

    login page styling.

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,37 +1,88 @@
 export default function Login() {
-    return (
-        <div className="form_container ">
-            <section id="login-page" className="auth">
-                <form id="create">
-                    <div className="container">
-                        <h1>Login</h1>
-                        <input
-                            type="text"
-                            id="title"
-                            name="title"
-                            placeholder="Enter username..."
-                        />
-                        <input
-                            type="text"
-                            id="title"
-                            name="title"
-                            placeholder="Enter title..."
-                        />
-                        <input
-                            type="text"
-                            id="password"
-                            name="password"
-                            placeholder="password title..."
-                        />
-                        <input
-                            className="btn-submit"
-                            id="submit"
-                            type="submit"
-                            value="Login"
-                        />
+  return (
+    <div className="form_container">
+      <div class="pt-5">
+        <h1 class="text-center">Login</h1>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-5 mx-auto">
+              <div class="card card-body">
+                <form
+                  id="submitForm"
+                  action="/login"
+                  method="post"
+                  data-parsley-validate=""
+                  data-parsley-errors-messages-disabled="true"
+                  novalidate=""
+                  _lpchecked="1"
+                >
+                  <input
+                    type="hidden"
+                    name="_csrf"
+                    value="7635eb83-1f95-4b32-8788-abec2724a9a4"
+                  />
+                  <div class="form-group required">
+                    <lsabel for="username">Email</lsabel>
+                    <input
+                      type="text"
+                      class="form-control text-lowercase"
+                      id="username"
+                      required=""
+                      name="username"
+                      value=""
+                    />
+                  </div>
+                  <div class="form-group required">
+                    <label
+                      class="d-flex flex-row align-items-center"
+                      for="password"
+                    >
+                      Password
+                      <a
+                        class="ml-auto border-link small-xl"
+                        href="/forget-password"
+                      >
+                        Forget?
+                      </a>
+                    </label>
+                    <input
+                      type="password"
+                      class="form-control"
+                      required=""
+                      id="password"
+                      name="password"
+                      value=""
+                    />
+                  </div>
+                  <div class="form-group mt-4 mb-4">
+                    <div class="custom-control custom-checkbox">
+                      <input
+                        type="checkbox"
+                        class="custom-control-input"
+                        id="remember-me"
+                        name="remember-me"
+                        data-parsley-multiple="remember-me"
+                      />
+                      <label class="custom-control-label" for="remember-me">
+                        Remember me?
+                      </label>
                     </div>
+                  </div>
+                  <div class="form-group pt-1">
+                    <button class="btn btn-primary btn-block" type="submit">
+                      Log In
+                    </button>
+                  </div>
                 </form>
-            </section>
+                <p class="small-xl pt-3 text-center">
+                  <span class="text-muted">Not a member?</span>
+                  <a href="/signup">Sign up</a>
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
